a343332a6ea5a36de20e9d7fea70fd92d62dcae4
unknown <heinhtetaung5698@gmail.com>
2026-05-08 21:45:53 +0700

    Redirect log in page to experience Hub

--- app/credentials-cloud/page.tsx
+++ app/credentials-cloud/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function CredentialsCloudEntryPage() {
-  redirect("/credentials-cloud/credentials");
+  redirect("/experience-hub");
 }
